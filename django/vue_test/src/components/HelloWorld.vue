07591dafb1d556012e9813df3ea35fc5e75e9398
OctopusLian <zoctopus@qq.com>
2022-04-10 15:36:20 +0800

    vue练习

---
+++ django/vue_test/src/components/HelloWorld.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="hello">
+    <h1>组件通信</h1>
+    <p>用户名： {{username}}</p>
+    <input type="text" v-model="name">
+    <input type="button" @click="submit" value="提交">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  // props: ['username'],
+  props: {
+    username: {
+      type: String,
+      required: true,
+      default: 'admin'
+    }
+  },
+  data() {
+    return {
+      name: ''
+    }
+  },
+  methods: {
+    submit() {
+      this.$emit('myEvent', {name: this.name}) 
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+}
+.text-danger {
+  color: #f00;
+}
+.panel {
+  background-color: aquamarine;
+  padding: 50px;
+  p {
+    background-color: #fff;
+    padding: 20px;
+  }
+}
+
+</style>
---
+++ django/vue_test/src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld v-bind:username="username"/>
+    <HelloWorld v-on:my-event="dealEvent"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      username: '张三'
+    }
+  },
+  methods: {
+    dealEvent(data) {
+      console.log('从子组件传递过来的数据：', data)
+    }
+  }
+}
+</script>
